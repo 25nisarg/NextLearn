11ca7768364165e0d867bd96142b79a43f65815f
Nisarg Mistry <mistry.nisarg.24@gmail.com>
2025-11-19 17:24:45 +0530

    Image, Script and linkTag in next

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-around bg-slate-800 text-white py-4'>
+      <div className="text-center">Copyright | All rights reserved</div>
+    </footer>
+  )
+}
+
+export default Footer
